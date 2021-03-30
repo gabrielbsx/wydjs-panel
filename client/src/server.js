c540169e8cc4a47d31f9da59645e71737e5b9718
Gabrielbsx <gabrielturing@gmail.com>
2021-03-29 22:14:51 -0300

    Views

--- client/src/server.js
+++ client/src/server.js
@@ -1,11 +1,16 @@
-const   express     = require("express"),
-        cors        = require("cors"),
-        morgan      = require("morgan"),
+const   express     = require('express'),
+        cors        = require('cors'),
+        morgan      = require('morgan'),
         helmet      = require('helmet'),
-        compression = require("compression"),
-        jwt         = require('jsonwebtoken');
+        compression = require('compression'),
+        jwt         = require('jsonwebtoken'),
+        ejs         = require('ejs'),
+        layouts     = require('express-ejs-layouts'),
+        flash       = require('express-flash-messages'),
+        path        = require('path')
+        routes      = require('./routes');
 
-require("dotenv").config();
+require('dotenv').config();
 app = express();
 
 app.use(cors());
@@ -15,8 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 app.set('trust proxy', 1);
-app.use(require('./routes'));
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.use(layouts);
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    console.log(`Server is running on https://localhost:${process.env.PORT}`);
 });
