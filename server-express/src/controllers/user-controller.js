ab170d30b2c32ba7b91980ee596405e116c15e39
Gabriel <gabrielturing@gmail.com>
2021-04-10 03:06:42 -0300

    schemas joi

--- server-express/src/controllers/user-controller.js
+++ server-express/src/controllers/user-controller.js
@@ -1,61 +1,6 @@
 const Joi = require('joi');
-const { max } = require('../models/users-model');
-
-const loginSchema = Joi.object({
-    name: Joi.string()
-        .max(255)
-        .required()
-        .messages({
-            'string.base': 'Nome deve conter apenas caracteres alfa numéricos!',
-            'string.empty': 'Nome não deve estar vázio!',
-            'string.max': 'Nome muito grande!',
-            'string.required': 'Nome obrigatório!',
-        }),
-
-    username: Joi.string()
-        .alphanum()
-        .min(4)
-        .max(12)
-        .required()
-        .messages({
-            'string.base': 'Usuário deve conter apenas caracteres alfa numéricos!',
-            'string.empty': 'Usuário não deve estar vázia!',
-            'string.alphanum': 'Usuário deve conter apenas caracteres alfa numéricos!',
-            'string.min': 'Usuário deve conter no mínimo 4 caracteres!',
-            'string.max': 'Usuário deve conter no máximo 12 caracteres!',
-            'string.required': 'Usuário obrigatório!',
-        }),
-
-    password: Joi.string()
-        .alphanum()
-        .min(4)
-        .max(12)
-        .required()
-        .messages({
-            'string.base': 'Senha deve conter apenas caracteres alfa numéricos!',
-            'string.empty': 'Senha não deve estar vázia!',
-            'string.alphanum': 'Senha deve conter apenas caracteres alfa numéricos!',
-            'string.min': 'Senha deve conter no mínimo 4 caracteres!',
-            'string.max': 'Senha deve conter no máximo 12 caracteres!',
-            'string.required': 'Senha obrigatória!',
-        }),
-
-    password_confirm: Joi.valid(Joi.ref('password'))
-        .required()
-        .messages({
-            'string.required': 'Confirmação de senha obrigatória!',
-            'string.only': 'As senhas não são idênticas!',
-        }),
-
-    email: Joi.string()
-        .email({ minDomainSegments: 2, })
-        .required()
-        .messages({
-            'string.empty': 'E-mail não deve estar vázia!',
-            'string.required': 'E-mail obrigatório!',
-        }),
-        
-});
+const userSchema =  require('../schemas/users-schema');
+const registerSchema = userSchema.register;
 
 exports.login = async (req, res, next) => {
     try {
@@ -102,10 +47,10 @@ exports.create = async (req, res, next) => {
                     password_confirm: password_confirm,
                     email: email,
                 };
-                const validate = await loginSchema.validateAsync(user);
+                const validate = await registerSchema.validateAsync(user, { abortEarly: false, });
                 req.flash('notify', {
                     type: 'success',
-                    message: user,
+                    message: 'Conta criada com sucesso!',
                 });
             } catch (err) {
                 req.flash('notify', {
@@ -116,7 +61,6 @@ exports.create = async (req, res, next) => {
         }
         return res.redirect('/register');
     } catch (err) {
-        console.log(err);
         return res.status(500).render('dashboard/pages/internalError', { err: err, });
     }
 }
