e883d4004160ca23d051b95560a6770ec81dfb68
Gabriel <gabrielturing@gmail.com>
2021-04-18 01:00:31 -0300

    donate and bonus

---
+++ monolith/src/models/paymentgateway-model.js
@@ -0,0 +1,24 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+class PaymentGateway extends Model {
+  static init(sequelize) {
+    super.init({
+        name: DataTypes.STRING(100),
+        key: DataTypes.STRING(255),
+        token: DataTypes.STRING(255),
+        created_at: DataTypes.DATE,
+        updated_at: DataTypes.DATE,
+    }, {
+      sequelize,
+      modelName: 'payment_gateway'
+    });
+    return this;
+  }
+
+  static associate(models) {
+    //this.belongsToMany(model.User, { foreignKey: 'id_user', as: 'owner' });
+  }
+}
+  
+module.exports = PaymentGateway;
