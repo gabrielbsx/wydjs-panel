5f7ab273d6b88e0ed50c883ea848cc61e1072085
Gabriel <gabrielturing@gmail.com>
2021-05-18 02:44:41 -0300

    gateways db separate

---
+++ monolith/src/database/migrations/20210518053653-picpay_gateway.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return await queryInterface.createTable('picpay_gateway', {
+      xpicpaytoken: {
+        type: Sequelize.STRING(),
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      xsellertoken: {
+        type: Sequelize.STRING(),
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.dropTable('picpay_gateway');
+  }
+};
---
+++ monolith/src/database/migrations/20210518053702-mercadopago_gateway.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return await queryInterface.createTable('mercadopago_gateway', {
+      public_key: {
+        type: Sequelize.STRING(),
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      access_token: {
+        type: Sequelize.STRING(),
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.dropTable('mercadopago_gateway');
+  }
+};
